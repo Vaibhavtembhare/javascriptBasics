7425b58fdef44833a576c30403159facda6c7b97
Vaibhavtembhare <60599888+Vaibhavtembhare@users.noreply.github.com>
2023-09-26 06:00:46 +0000

    done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";       // treat all JS code as newer version
+
+// ctrl + shift + ` : open terminal
+
+// alert("hello")   // we are using nodejs, not browser
+
+console.log(3 
+    +3 )  
+
+    // code readability should be high
+
+console.log("vaibhav")
+
+
+let name="vaibhav"
+let age=24
+let isLoggedIn=false
+
+// number : 2 to power 53
+// bigint
+// string : ""
+// boolean : true/false
+// null  : standalone value
+// undefinded : not defined value
+// symbol : to represent uniqueness
+// object : 
+
+console.log(typeof "vaibhav");
+console.log(typeof null);           // null is an object in js  (interview question)
+console.log(typeof undefined)
