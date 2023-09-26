6d98d213fead367dbf51c98257f75d038b97241b
Vaibhavtembhare <60599888+Vaibhavtembhare@users.noreply.github.com>
2023-09-26 07:28:33 +0000

    done

---
+++ 01_basics/04_Operations.js
@@ -0,0 +1,30 @@
+let value=3
+let negValue=-value
+// console.log(negValue);  // -3
+
+let str1="hello"
+let str2=" vaibhav"
+
+let str3=str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+//console.log(+true);
+//console.log(true+);
+//console.log(+"")
+
+let num1, num2, num3
+
+num1=num2=num3=2+2
+
+//console.table([num1,num2,num3]);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
